0a619719880a47a9f24112eae39b7bb64417c047
A. D. Barwell <adam.barwell.09@ucl.ac.uk>
2013-01-28 01:24:23 +0000

    removed old database helper; commented out mention of old database helper

--- src/com/webviewprototype/database/testactivities/TestActivity.java
+++ src/com/webviewprototype/database/testactivities/TestActivity.java
@@ -1,17 +1,11 @@
 package com.webviewprototype.database.testactivities;
 
 import android.app.Activity;
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.webviewprototype.R;
-import com.webviewprototype.database.DBContract;
-import com.webviewprototype.database.DBHelper;
 
 public class TestActivity extends Activity {
 	
@@ -24,40 +18,40 @@ public class TestActivity extends Activity {
     
     public void ping(View view) {
     	(Toast.makeText(this, "Pong", Toast.LENGTH_SHORT)).show();
-    	add();
-    	read();
+//    	add();
+//    	read();
     }
     
-    private void read() {
-    	Log.d(log_tag, "getting Database");
-    	SQLiteDatabase db = new DBHelper(this).getReadableDatabase();
-    	
-    	Log.d(log_tag, "querying Database");
-    	String query = "SELECT * FROM " + DBContract.FormTable.TABLE_NAME;
-    	Cursor cursor = db.rawQuery(query, null);
-    	Log.d(log_tag, "reading entries");
-    	Log.d(log_tag, "Cursor length: " + Integer.toString(cursor.getCount()));
-    	if (cursor.getCount() > 0) {
-	    	cursor.moveToFirst();
-	    	do {
-	    		Long form_id = cursor.getLong(cursor.getColumnIndexOrThrow(DBContract.FormTable._ID));
-	    		Log.i(log_tag, Long.toString(form_id));
-	    	} while (cursor.moveToNext());
-    	}
-    }
-    
-    private void add() {
-    	Log.d(log_tag, "getting Database");
-    	SQLiteDatabase db = new DBHelper(this).getWritableDatabase();
-    	
-    	Log.d(log_tag, "adding to Database");
-    	ContentValues values = new ContentValues();
-    	values.put(DBContract.FormTable.COLUMN_NAME_FORM_ID, 3);
-    	values.put(DBContract.FormTable.COLUMN_NAME_FORM_TYPE_ID, 3);
-    	values.put(DBContract.FormTable.COLUMN_NAME_URL, "www.google.co.uk");
-    	
-    	long newRowId = db.insert(DBContract.FormTable.TABLE_NAME, null, values);
-    	Log.i(log_tag, "insert successful, newRowId: " + Long.toString(newRowId));
-    }
+//    private void read() {
+//    	Log.d(log_tag, "getting Database");
+//    	SQLiteDatabase db = new DBHelper(this).getReadableDatabase();
+//    	
+//    	Log.d(log_tag, "querying Database");
+//    	String query = "SELECT * FROM " + DBContract.FormTable.TABLE_NAME;
+//    	Cursor cursor = db.rawQuery(query, null);
+//    	Log.d(log_tag, "reading entries");
+//    	Log.d(log_tag, "Cursor length: " + Integer.toString(cursor.getCount()));
+//    	if (cursor.getCount() > 0) {
+//	    	cursor.moveToFirst();
+//	    	do {
+//	    		Long form_id = cursor.getLong(cursor.getColumnIndexOrThrow(DBContract.FormTable._ID));
+//	    		Log.i(log_tag, Long.toString(form_id));
+//	    	} while (cursor.moveToNext());
+//    	}
+//    }
+//    
+//    private void add() {
+//    	Log.d(log_tag, "getting Database");
+//    	SQLiteDatabase db = new DBHelper(this).getWritableDatabase();
+//    	
+//    	Log.d(log_tag, "adding to Database");
+//    	ContentValues values = new ContentValues();
+//    	values.put(DBContract.FormTable.COLUMN_NAME_FORM_ID, 3);
+//    	values.put(DBContract.FormTable.COLUMN_NAME_FORM_TYPE_ID, 3);
+//    	values.put(DBContract.FormTable.COLUMN_NAME_URL, "www.google.co.uk");
+//    	
+//    	long newRowId = db.insert(DBContract.FormTable.TABLE_NAME, null, values);
+//    	Log.i(log_tag, "insert successful, newRowId: " + Long.toString(newRowId));
+//    }
 
 }
